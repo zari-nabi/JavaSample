edab549fb67ff3cae59f601d332ce188bb578330
zari-nabi <m.nabi.zahra@gmail.com>
2023-11-29 22:26:24 +0200

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * This code prints "Hello World!"
+ */
+public class HelloWorld {
+
+    /**
+     *
+     * @param args an arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
